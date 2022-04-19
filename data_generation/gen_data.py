1f32f1642ba79257ae4222283cab90593eb397ac
Nitheesh K L <nitheeshkl@gmail.com>
2022-04-19 16:22:22 -0400

    render keypoints from keypointnet dataset

--- data_generation/gen_data.py
+++ data_generation/gen_data.py
@@ -1,10 +1,14 @@
 import sys
 import os
 import numpy as np
+from sklearn import model_selection
 import torch
 from tqdm.auto import tqdm
+import json
 
 from pytorch3d.io import load_objs_as_meshes
+from pytorch3d.io import IO
+from pytorch3d.structures import Meshes, Pointclouds
 from pytorch3d.renderer import (
     look_at_view_transform,
     FoVPerspectiveCameras,
@@ -13,6 +17,11 @@ from pytorch3d.renderer import (
     MeshRasterizer,
     MeshRenderer,
     SoftPhongShader,
+    PointsRasterizationSettings,
+    PointsRenderer,
+    PulsarPointsRenderer,
+    PointsRasterizer,
+    AlphaCompositor,
 )
 from pytorch3d.vis.plotly_vis import plot_scene
 from PIL import Image
@@ -27,29 +36,85 @@ else:
     device = torch.device("cpu")
 
 # setup data
-DATA_DIR = "./data/"
-obj_file = os.path.join(DATA_DIR, "cow.obj")
+# --------- obj + mtl filetype ------------#
+# DATA_DIR = "./data/"
+# obj_file = os.path.join(DATA_DIR, "cow.obj")
+
+# ---------- ply filetype ----------------#
+DATA_DIR = "/home/kln/sandbox/cmu/repos/3d_project/dataset/keypointnet/"
+# model_file = "03001627/8d458ab12073c371caa2c06fded3ca21.ply"
+# ply_file = os.path.join(DATA_DIR, model_file)
+
+
+def load_keypoints(kpts_file):
+    model_file, kpts_xyz, kpts_rgb = None, None, None
+    with open(kpts_file) as f:
+        kpts_dict = json.load(f)[0]  # load only first keypoint
+
+        model_file = os.path.join(kpts_dict["class_id"], kpts_dict["model_id"] + ".ply")
+        kpts_xyz = []
+        kpts_rgb = []
+        for kpt in kpts_dict["keypoints"]:
+            kpts_xyz.append(kpt["xyz"])
+            kpts_rgb.append(kpt["rgb"])
+        kpts_xyz = torch.tensor(kpts_xyz)
+        kpts_rgb = torch.tensor(kpts_rgb)
+
+    return model_file, kpts_xyz, kpts_rgb
+
+
+kpts_file = os.path.join(DATA_DIR, "chair.json")
+model_file, kpts_xyz, kpts_rgb = load_keypoints(kpts_file)
+ply_file = os.path.join(DATA_DIR, "ShapeNetCore.v2.ply", model_file)
+
+# kpts_xyz = kpts_xyz * torch.tensor([-1, 1, -1])  # transform axis to pytorch3d
+kpts_xyz, kpts_rgb = kpts_xyz.to(device), kpts_rgb.to(device)
+
+# kpts_xyz = torch.tensor(
+#     [
+#         [0.16172105225242855, 0.39077869068739557, 0.1352145785287725],
+#         [-0.15787448163662027, 0.40384870078270607, 0.1307143438565415],
+#     ]
+# ).to(device)
 
 # load mesh obj
-mesh = load_objs_as_meshes([obj_file], device=device)
+# mesh = load_objs_as_meshes([obj_file], device=device)
+mesh = IO().load_mesh(ply_file, device=device)
 
 # We scale normalize and center the target mesh to fit in a sphere of radius 1
 # centered at (0,0,0). (scale, center) will be used to bring the predicted mesh
 # to its original center and scale.  Note that normalizing the target mesh,
 # speeds up the optimization but is not necessary!
-verts = mesh.verts_packed()
-N = verts.shape[0]
-center = verts.mean(0)
-scale = max((verts - center).abs().max(0)[0])
-mesh.offset_verts_(-center)
-mesh.scale_verts_((1.0 / float(scale)))
+def normalize_mesh(mesh):
+    verts = mesh.verts_packed()
+    N = verts.shape[0]
+    center = verts.mean(0)
+    scale = max((verts - center).abs().max(0)[0])
+    mesh.offset_verts_(-center)
+    mesh.scale_verts_((1.0 / float(scale)))
+    return mesh
+
+
+# mesh = normalize_mesh(mesh)
+
+
+def normalize_kpts(kpts_xyz):
+    N = kpts_xyz.shape[0]
+    center = kpts_xyz.mean(0)
+    scale = max((kpts_xyz - center).abs().max(0)[0])
+    kpts_xyz = kpts_xyz - center
+    kpts_xyz = kpts_xyz * (1.0 / float(scale))
+    return kpts_xyz
+
+
+# kpts_xyz = normalize_kpts(kpts_xyz)
 
 # number of sample outputs to generate
-train_samples = 80
-test_samples = 20
+train_samples = 8
+test_samples = 2
 num_samples = train_samples + test_samples
 
-image_size = 256
+image_size = 1024
 
 cam_dist = 3.0
 # Get a batch of viewing angles.
@@ -69,32 +134,80 @@ lights = PointLights(device=device, location=[[0.0, 0.0, -cam_dist]])
 R, T = look_at_view_transform(dist=cam_dist, elev=elev, azim=azim)
 cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
 
-# Create a Phong renderer by composing a rasterizer and a shader. The textured
-# Phong shader will interpolate the texture uv coordinates for each vertex,
-# sample from a texture image and apply the Phong lighting model
-renderer = MeshRenderer(
-    rasterizer=MeshRasterizer(
-        raster_settings=RasterizationSettings(
-            image_size=image_size,
-            blur_radius=0.0,
-            faces_per_pixel=1,
-        )
-    ),
-    shader=SoftPhongShader(device=device, lights=lights),
-)
 
-# Create a batch of meshes by repeating the mesh and associated textures.
-# Meshes has a useful `extend` method which allows us do this very easily.
-# This also extends the textures.
-meshes = mesh.extend(num_samples)
+def gen_kpts_images():
 
-# Render the mesh from each viewing angle
-target_images = renderer(meshes, cameras=cameras, lights=lights)
+    # kpts_pcd = Pointclouds(points=[kpts_xyz], features=[(kpts_rgb * 255) / 255.0])
+    kpts_features = torch.ones_like(kpts_xyz) * torch.tensor([255, 255, 255]).to(device)
+    kpts_pcd = Pointclouds(points=[kpts_xyz], features=[kpts_features])
 
-# generate dataset images
-for i, img in tqdm(enumerate(target_images.cpu().numpy())):
-    rgb_img = Image.fromarray((img[..., :3] * 255).astype(np.uint8))
-    rgb_img.save(f"./output/image_{i:06d}.png")
+    kpts_pcd = kpts_pcd.extend(num_samples)
+
+    points_renderer = PointsRenderer(
+        rasterizer=PointsRasterizer(
+            cameras=cameras,
+            raster_settings=PointsRasterizationSettings(
+                image_size=image_size, radius=0.003, points_per_pixel=10
+            ),
+        ),
+        compositor=AlphaCompositor(),
+    )
+
+    # generate keypoint images
+    kpts_images = points_renderer(kpts_pcd)
+    for i, img in tqdm(enumerate(kpts_images.cpu().numpy())):
+        img = (img[..., :3] * 255).astype(np.uint8)
+        rgb_img = Image.fromarray(img)
+        rgb_img.save(f"./output/kpts_{i:06d}.png")
+
+
+def gen_obj_images():
+
+    global kpts_rgb
+
+    kpts_xyz_batch = kpts_xyz.unsqueeze(0).repeat(num_samples, 1, 1)
+    kpts_screen = cameras.transform_points_screen(
+        kpts_xyz_batch, image_size=(image_size, image_size)
+    )
+    kpts_screen = kpts_screen.cpu().numpy()
+    kpts_rgb = kpts_rgb.cpu().numpy()
+
+    # Create a Phong renderer by composing a rasterizer and a shader. The textured
+    # Phong shader will interpolate the texture uv coordinates for each vertex,
+    # sample from a texture image and apply the Phong lighting model
+    mesh_renderer = MeshRenderer(
+        rasterizer=MeshRasterizer(
+            raster_settings=RasterizationSettings(
+                image_size=image_size, blur_radius=0.0, faces_per_pixel=1
+            )
+        ),
+        shader=SoftPhongShader(device=device, lights=lights),
+    )
+
+    # Create a batch of meshes by repeating the mesh and associated textures.
+    # Meshes has a useful `extend` method which allows us do this very easily.
+    # This also extends the textures.
+    meshes = mesh.extend(num_samples)
+
+    # Render the mesh from each viewing angle
+    target_images = mesh_renderer(meshes, cameras=cameras, lights=lights)
+
+    # generate dataset images
+    for i, img in tqdm(enumerate(target_images.cpu().numpy())):
+        img = (img[..., :3] * 255).astype(np.uint8)
+        kpts = kpts_screen[i].round().astype(int)
+        for j, kpt in enumerate(kpts):
+            # img[kpt[1], kpt[0]] = kpts_rgb[j]
+            r, c = kpt[1], kpt[0]
+            img[r - 2 : r + 2, c - 2 : c + 2, :] = np.array([255, 0, 0])
+            print(r, c)
+        rgb_img = Image.fromarray(img)
+        rgb_img.save(f"./output/image_{i:06d}.png")
+
+
+gen_kpts_images()
+gen_obj_images()
+
 
 # save pose information for dataset
 pose = torch.stack([elev, azim, torch.ones(num_samples) * cam_dist]).cpu().numpy().T
@@ -108,5 +221,5 @@ np.save("./output/train_indices.npy", train_idx)
 np.save("./output/test_indices.npy", test_idx)
 
 # DEBUG: show scene rendering for debugging
-# scene = plot_scene({"figure": {"Mesh": mesh, "Camera": cameras}})
-# scene.show()
+scene = plot_scene({"figure": {"Mesh": mesh, "Camera": cameras}})
+scene.show()
